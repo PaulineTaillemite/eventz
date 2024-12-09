47dd230a13cb71a918cde7d116025e8b022f3434
PaulineTaillemite <pauline.taillemite@gmail.com>
2024-12-09 10:35:29 +0100

    add update method in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,4 +10,11 @@ class EventsController < ApplicationController
     def edit
       @event = Event.find(params[:id])
     end
+
+    #we find the event and then use the update method (same as in the console) fetching the hash
+    def update
+      @event = Event.find(params[:id])
+      #this will update all the attributes
+      @event.update(params[:event])
+    end
 end
