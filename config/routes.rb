b8161c45ba275b3e5f6609dc3e0239cb0ec2f16e
PaulineTaillemite <pauline.taillemite@gmail.com>
2024-12-10 09:13:21 +0100

    stop

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
   #replace the main page
   root "events#index"
 
-  get "events" => "events#index"
-  #using :id as a variable
-  get"events/:id"=>"events#show", as: "event"
-  get "events/:id/edit"=>"events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  resources :events
 end
