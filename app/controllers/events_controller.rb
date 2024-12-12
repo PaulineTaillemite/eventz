0a7fc64b919d10e2a96e4fc4c08a6bb7f882b1eb
PaulineTaillemite <pauline.taillemite@gmail.com>
2024-12-12 10:52:41 +0100

    add private method in controller to avoid duplication

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,6 @@ class EventsController < ApplicationController
     #we find the event and then use the update method (same as in the console) fetching the hash
     def update
       @event = Event.find(params[:id])
-      event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
       @event.update(event_params)
       redirect_to event_path(@event)
       #or redicect_to @event
@@ -26,9 +25,14 @@ class EventsController < ApplicationController
     end
 
     def create
-      event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
       @event = Event.new(event_params)
       @event.save
       redirect_to @event
+    end
+
+  private
+    def event_params
+      params.require(:event).
+        permit(:name, :description, :location, :price, :starts_at)
     end
 end
