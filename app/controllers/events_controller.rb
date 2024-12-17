a3370adf5fd493c119dde93d5d069a98642d2a7f
PaulineTaillemite <pauline.taillemite@gmail.com>
2024-12-17 15:35:44 +0100

    if in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,8 +26,11 @@ class EventsController < ApplicationController
 
     def create
       @event = Event.new(event_params)
-      @event.save
+      if @event.save
       redirect_to @event
+      else 
+        render :new, status: :unprocessable_entity
+      end
     end
 
     def destroy
