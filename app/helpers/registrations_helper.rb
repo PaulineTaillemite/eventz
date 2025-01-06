5e498cc7c7baed180cae368f1ec3311f35c3dcef
PaulineTaillemite <pauline.taillemite@gmail.com>
2025-01-06 09:03:14 +0100

    set registration helper

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,11 @@
 module RegistrationsHelper
+
+  def register_or_sold_sold (event)
+    if event.sold_out?
+      "Sold out!"
+    else 
+      link_to "Register!", new_event_registration_path(event), class: "register"
+    end
+
+  end
 end
