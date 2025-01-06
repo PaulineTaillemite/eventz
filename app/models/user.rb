b5e68e6c42dbea3223fe474c92ede5638b79b9f5
PaulineTaillemite <pauline.taillemite@gmail.com>
2025-01-06 10:20:33 +0100

    create and test password on console

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  validates : email, format: { with: /\S+@\S+/ }, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /\S+@\S+/ }, presence: true, uniqueness: { case_sensitive: false }
 
 
 end
