f9c9b1eb668ebc6de179411cf68901c8e5416b5b
PaulineTaillemite <pauline.taillemite@gmail.com>
2025-01-07 10:55:32 +0100

    def create method with authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    user = User.find_by(email: params[:email])
+    #if user not nil and authenficate returns true
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
