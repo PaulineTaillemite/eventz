a182054f7e840b8a59cbfdfb9b466bf3fd35f0c6
PaulineTaillemite <pauline.taillemite@gmail.com>
2025-01-08 14:19:44 +0100

    create the current user method to call in header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def current_user
+    #if the user is not signed in query will return nil so we need to check if the session has a uder id
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
