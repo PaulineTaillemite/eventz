e620beeb4fe065c46b407d6758bf96a0908129df
PaulineTaillemite <pauline.taillemite@gmail.com>
2025-01-09 12:26:26 +0100

    migration for admin column in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_06_082940) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_09_111134) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_06_082940) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "registrations", "events"
